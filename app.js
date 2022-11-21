67f7802164eabd8c92714670fd63c319d879afff
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-11-20 17:18:46 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'                                                             // import the new cors library
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js";               // import controller
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+
+
+const app = express();
+app.use(cors())                 // configure cors right after instantiating express
+app.use(express.json());        // parse JSON from HTTP request body
+TuitsController(app);
+HelloController(app)
+UserController(app)             // pass it app
+app.listen(4000)
---
+++ controllers/users/users-controller.js
@@ -0,0 +1,54 @@
+import people from './users.js'     // import the array of users
+let users = people
+
+const findUsers = (req, res) => {                   // function runs when /api/users requested
+    const type = req.query.type                     // retrieve type parameter from query
+    if (type) {                                     // find users of that type
+        const usersOfType = users.filter(u => u.type === type)
+        res.json(usersOfType)                       // respond with users of that type
+        return                                      // return so it doesn't continue
+    }
+    res.json(users)                                 // otherwise respond with all users
+}
+
+const UserController = (app) => {                   // use express instance app to declare HTTP GET
+    app.get('/api/users', findUsers)                // request pattern /api/users to call a function
+    app.get('/api/users/:uid', findUserById);       // map path pattern to handler function
+    app.post('/api/users', createUser);             // map URL pattern to handler function
+    app.delete('/api/users/:uid', deleteUser);      // map URL pattern to handler function
+    app.put('/api/users/:uid', updateUser);
+}
+
+const createUser = (req, res) => {                  // function invoked if URL matches pattern
+    const newUser = req.body;                       // extract new user from BODY in request
+    newUser._id = (new Date()).getTime() + '';      // add an _id property with unique timestamp
+    users.push(newUser);                            // append new user to users array
+    res.json(newUser);                              // respond with new user to client
+}
+
+const findUserById = (req, res) => {                // function called if URL matches pattern
+    const userId = req.params.uid;                  // get uid from request parameter map
+    const user = users                              // find user in users array whose _id
+        .find(u => u._id === userId);               // matches userId retrieved from params
+    res.json(user);                                 // respond to client with user found
+}
+
+const deleteUser = (req, res) => {
+    const userId = req.params['uid'];   // get user ID from path parameter uid
+    users = users.filter(usr =>         // filter out the user
+        usr._id !== userId);            // whose ID is the ID of the user we want to remove
+    res.sendStatus(200);                // respond with success code
+}
+
+const updateUser = (req, res) => {      // handle PUT /api/users/:uid
+    const userId = req.params['uid'];   // get user ID from path
+    const updates = req.body;           // BODY includes updated fields
+    users = users.map((usr) =>     // create a new array of users
+        usr._id === userId ?            // if current user's ID matches ID we want to update
+            {...usr, ...updates} :      // merge old usr with new updates
+            usr                         // otherwise keep the old user
+    );
+    res.sendStatus(200);                // return OK
+}
+
+export default UserController           // exports so app.js can import
