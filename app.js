983acc839a36bed4c08714e523b5c0682fa4b749
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-11-29 23:59:11 -0800

    update server

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
+// 最开始只在local运行的时候 localmongoose.connect('mongodb://localhost:27017/tuiter');
+// 设然后设置environment variable DB_CONNECTION_STRING=mongodb+srv://8orangemoon:<password>@cluster0.jnidktq.mongodb.net/?retryWrites=true&w=majority
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
-// mongoose.connect('mongodb://localhost:27017/tuiter');
-// mongodb+srv://8orangemoon:<password>@cluster0.jnidktq.mongodb.net/?retryWrites=true&w=majority
 
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors())                 // configure cors right after instantiating express
