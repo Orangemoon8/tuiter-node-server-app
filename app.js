ad18ce325e996c4cf34ecb8ee5651b6bb34cfef8
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-11-29 22:12:04 -0800

    finish A9 before deploy

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect('mongodb://localhost:27017/tuiter');
+// mongodb+srv://8orangemoon:<password>@cluster0.jnidktq.mongodb.net/?retryWrites=true&w=majority
+
 
 const app = express();
 app.use(cors())                 // configure cors right after instantiating express
